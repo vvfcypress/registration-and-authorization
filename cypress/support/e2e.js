faf6bae7a54ad759943dc0298f059e0a59337e12
vvfcypress <vvfcypress@gmail.com>
2023-11-13 17:11:47 +0200

    added Order test

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './helper'
+import './helpers/productHelper'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
